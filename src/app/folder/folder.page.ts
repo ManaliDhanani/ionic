bb2af485bd2a3c61cdc643ccf7fb830672ad1106
ManaliDhanani <manalidhanani105@gmail.com>
2024-05-02 17:47:28 +0530

    change

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,10 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public results = [...this.data];
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  handleInput(value) {
+    const query = value.toLowerCase();
+    this.results = this.data.filter((d) => d.toLowerCase().includes(value));
   }
 
   togglePasswordVisibility(input: any){
