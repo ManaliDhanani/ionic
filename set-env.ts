850837a309ae49b61cdb62e86a8aad5c58a4b90e
ManaliDhanani <manalidhanani105@gmail.com>
2025-04-28 15:43:34 +0530

    set-env setup

---
+++ set-env.ts
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+require('dotenv').config();
+
+console.log('MAPS_KEY:', process.env['MAPS_KEY']);
+console.log('FIREBASE_API_KEY:', process.env['FIREBASE_API_KEY']);
+
+const targetPath = path.resolve(__dirname, 'src/environments/environment.ts');
+console.log('targetPath: ', targetPath);
+
+const environmentFileContent = `
+export const environment = {
+  production: false,
+  mapsKey: '${process.env['MAPS_KEY']}',
+  firebaseConfig: {
+    apiKey: '${process.env['FIREBASE_API_KEY']}',
+    authDomain: '${process.env['FIREBASE_AUTH_DOMAIN']}',
+    databaseURL: '${process.env['FIREBASE_DATABASE_URL']}',
+    projectId: '${process.env['FIREBASE_PROJECT_ID']}',
+    storageBucket: '${process.env['FIREBASE_STORAGE_BUCKET']}',
+    messagingSenderId: '${process.env['FIREBASE_MESSAGING_SENDER_ID']}',
+    appId: '${process.env['FIREBASE_APP_ID']}',
+    measurementId: '${process.env['FIREBASE_MEASUREMENT_ID']}'
+  }
+};
+`;
+
+fs.writeFileSync(targetPath, environmentFileContent);
+
+console.log(`✅ Environment file generated at ${targetPath}`);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,36 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  mapsKey : 'AIzaSyDvaS5zz8c4iSP8YS6BiXbWUMYsFuYwqmY',
+  mapsKey: 'undefined',
   firebaseConfig: {
-    apiKey: "AIzaSyDvaS5zz8c4iSP8YS6BiXbWUMYsFuYwqmY",
-    authDomain: "angularhttpclient-d6c80.firebaseapp.com",
-    databaseURL: "https://angularhttpclient-d6c80-default-rtdb.firebaseio.com",
-    projectId: "angularhttpclient-d6c80",
-    storageBucket: "angularhttpclient-d6c80.appspot.com",
-    messagingSenderId: "149263745013",
-    appId: "1:149263745013:web:6eaecaf441f81589182abc",
-    measurementId: "G-PKLX9XYX3W"
+    apiKey: 'undefined',
+    authDomain: 'undefined',
+    databaseURL: 'undefined',
+    projectId: 'undefined',
+    storageBucket: 'undefined',
+    messagingSenderId: 'undefined',
+    appId: 'undefined',
+    measurementId: 'undefined'
   }
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyBc5oPWgKTyEWABig8rAu9d_FVDvmkTfng",
-  //   authDomain: "angularhttpclient-d6c80.firebaseapp.com",
-  //   databaseURL: "https://angularhttpclient-d6c80-default-rtdb.firebaseio.com",
-  //   projectId: "angularhttpclient-d6c80",
-  //   storageBucket: "angularhttpclient-d6c80.appspot.com",
-  //   messagingSenderId: "149263745013",
-  //   appId: "1:149263745013:android:b954e7f3b0dc7f0e182abc",
-  // }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
